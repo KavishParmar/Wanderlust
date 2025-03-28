58de2fd8f548544dd30272a6c66031e5c6ef2d83
Kavish parmar <kavishparmar5@gmail.com>
2025-03-28 06:28:53 +0530

    Correcting mistake of adding anchor tag in title

--- public/js/script.js
+++ public/js/script.js
@@ -17,3 +17,19 @@
       }, false);
     });
 })()
+
+
+//  Toggle tax switch btn to show tax info
+    let taxSwitch = document.getElementById("flexSwitchCheckDefault");
+    taxSwitch.addEventListener('click',() => {
+      let taxInfo = document.querySelectorAll(".tax-info");
+      for ( info of taxInfo){
+        if (info.style.display != 'inline'){
+          info.style.display = 'inline';
+        }else{
+          info.style.display = 'none';          
+        }
+
+      }
+    })
+  
